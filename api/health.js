51c3b345f69ae75e31d9bd955fc5825094e55094
Shazaj <mohamed.amine.elkouraichi.10@gmail.com>
2026-07-31 12:46:21 +0100

    hooooooooo

--- api/health.js
+++ api/health.js
@@ -52,7 +52,7 @@ export default async function handler(req, res) {
     firebaseAdminInitialized = false;
   }
 
-  return sendJson(res, firebaseAdminInitialized ? 200 : 500, {
+  return sendJson(res, 200, {
     ok:
       environment.clerkSecretConfigured &&
       environment.firebaseProjectConfigured &&
